fce1a158d41c652f969b8427fda21bca3120f23e
rexx0n <alexey.mironenko46@gmail.com>
2023-05-18 12:56:50 +0300

    made block adn fixed on scroll header

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//Swith
 let btnUp = document.querySelector('.services__btn--up')
 let btnDown = document.querySelector('.services__btn--down')
 let ventilator = document.querySelector('.services__ventilator')
@@ -15,3 +16,22 @@ btnUp.addEventListener('click', ()=> {
     airConditioner.classList.add('services__none')
     ventilator.classList.remove('services__none')
 })
+//header scroll
+let header = document.querySelector('.header'),
+    headerH = document.querySelector('.header').clientHeight
+let main = document.querySelector('.main')
+document.onscroll = function () {
+    let scroll = window.scrollY
+    if (scroll > headerH) {
+        header.classList.add('header__scroll')
+        main.classList.add('main--pt')
+    }
+    else {
+        header.classList.remove('header__scroll')
+        main.classList.remove('main--pt')
+    }
+}
+// header.addEventListener('focusout', ()=> {
+//     header.classList.add('header__scroll')
+//     main.classList.add('main--pt')
+// })
